fd8e4b6e35a8828786bccb6ee6f469ab5a29f983
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2025-03-13 12:30:03 -0300

    baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "https://sales-manager-nodejs.onrender.com",
-//   timeout: 30000,
-// });
-
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "sales-manager-10-production.up.railway.app",
+  timeout: 30000,
 });
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000",
+// });
 
 export default api;
